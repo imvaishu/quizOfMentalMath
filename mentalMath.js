be5edfd57329353f1381585bea65821ea8f628f8
vaishnavi <58027647+imvaishu@users.noreply.github.com>
2019-12-04 17:01:20 +0530

    successfully exited
    What changes you have made?
    exception cases handled

--- mentalMath.js
+++ mentalMath.js
@@ -1,22 +1,33 @@
+const { stdin, exit } = process;
 const questionList = require("./questions.json");
-const { stdin, stdout, exit } = process;
-const displayQuestion = function(questionList, qNo) {
-  console.log(questionList[qNo].question);
+
+const displayQuestion = function(questionList, questionNo) {
+  console.log(questionList[questionNo].question);
 };
-const isCorrect = function(answer, questionList, qNo) {
-  const ans = +questionList[qNo].answer;
-  return ans == answer;
+
+const isCorrectAnswer = function(answer, questionList, qNo) {
+  return +questionList[qNo].answer == answer;
 };
+
 const main = function(questionList) {
-  let qNo = 0;
-  displayQuestion(questionList, qNo);
+  let questionNo = 0;
+  let score = 0;
+  displayQuestion(questionList, questionNo);
+
   stdin.on("data", answer => {
-    let message = "Incorrect Answer";
-    if (isCorrect(answer, questionList, qNo)) {
-      message = "Correct Answer";
+    let message = `Incorrect Answer\t Score: ${score}`;
+    if (isCorrectAnswer(answer, questionList, questionNo)) {
+      message = `Correct Answer\t Score: ${++score}`;
     }
+
     console.log(message);
-    displayQuestion(questionList, ++qNo);
+    if (questionNo == questionList.length - 1) {
+      console.log("Quiz Over");
+      exit();
+    }
+
+    displayQuestion(questionList, ++questionNo);
   });
 };
+
 main(questionList);
