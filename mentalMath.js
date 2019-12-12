330f7231219a0b854caa042f7eca3dd88e8d8be9
vaishnavi <58027647+imvaishu@users.noreply.github.com>
2019-12-12 14:50:30 +0530

    timer added for each question

--- mentalMath.js
+++ mentalMath.js
@@ -3,34 +3,46 @@ const questionList = require("./questions.json");
 const chalk = require("chalk");
 
 const displayQuestion = function(questionList, questionNo) {
-  console.log(chalk.yellow(questionList[questionNo].question));
+  console.log(chalk.black.bgCyan(questionList[questionNo].question));
 };
 
 const isCorrectAnswer = function(usrResponse, questionList, qNo) {
   return +questionList[qNo].answer == usrResponse;
 };
 
+const getMessage = function(usrResponse, questionList, questionNo, score) {
+  let message = `Incorrect Answer\t Score: ${score}`;
+  message = chalk.red(message);
+  if (isCorrectAnswer(usrResponse, questionList, questionNo)) {
+    message = `Correct Answer\t Score: ${++score}`;
+    message = chalk.green(message);
+  }
+  return message;
+};
+
+const timer = function() {
+  return setTimeout(() => {
+    console.log("Time Out");
+    stdin.emit("data");
+  }, 30000);
+};
+
 const main = function(questionList) {
   let questionNo = 0;
   let score = 0;
   displayQuestion(questionList, questionNo);
+  timer();
 
   stdin.setEncoding("utf-8");
   stdin.on("data", usrResponse => {
-    let message = `Incorrect Answer\t Score: ${score}`;
-    message = chalk.red(message);
-    if (isCorrectAnswer(usrResponse, questionList, questionNo)) {
-      message = `Correct Answer\t Score: ${++score}`;
-      message = chalk.green(message);
-    }
-
+    const message = getMessage(usrResponse, questionList, questionNo, score);
     console.log(message);
     if (questionNo == questionList.length - 1) {
       console.log(chalk.magenta("Quiz Over"));
       exit();
     }
 
-    displayQuestion(questionList, ++questionNo);
+    timer(displayQuestion(questionList, ++questionNo));
   });
   setTimeout(() => {
     console.log("Time Out");
